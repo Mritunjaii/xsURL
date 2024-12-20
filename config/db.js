f5a25de9349b017fe1d01a5c7b579fec1edf77ea
Mritunjai <101278178+Mritunjaii@users.noreply.github.com>
2024-12-20 16:54:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 function connectToDb(){
-    mongoose.connect(process.env.MONGO_URL).then(()=>{
+    mongoose.connect(process.env.MONGO_URL.replace('<PASSWORD>',process.env.DB_PASSWORD)).then(()=>{
         console.log("Connected to db");
     })
 }
